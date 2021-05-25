918d607e4a19d748b56a2b4a9ebde9b1b2e5c521
Badr-Ba-Sowid <61441185+Badr-Ba-Sowid@users.noreply.github.com>
2021-05-25 13:35:01 +0800

    project created

---
+++ app/src/main/java/com/example/kmeproject/inferanceEngine.java
@@ -0,0 +1,27 @@
+package com.example.kmeproject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class inferanceEngine {
+    private final static String CLASSIC = "CLASSIC";
+
+
+    // this method is going to be called from the ui
+    // to make inferances , the output should be
+    // a single string of one of the categories
+
+//
+//    private String infer(){
+//        HashMap<String , boolean> input = new HashMap();
+//        input.put(CLASSIC , false );
+//
+//        if(input.get(SIMPLE_ELEMENTS) && input.get(AVERAGE) ) {
+//            return MODERN;
+//        }else{
+//
+//        }
+//    }
+
+
+}
